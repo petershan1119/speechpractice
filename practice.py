2304c836ac57d4b1c177a2892664c3c41c46e0b9
petershan1119 <petershan1119@gmail.com>
2019-11-14 14:31:06 +0900

    second commit

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
 import re
 
 import nltk
+import pandas as pd
+import numpy as np
 
 from nltk.corpus import gutenberg
 from string import punctuation
@@ -24,11 +26,43 @@ def normalize_document(doc):
     doc = ' '.join(filtered_tokens)
     return doc
 
+normalize_corpus = np.vectorize(normalize_document)
+
+
 
 
 
 norm_bible = [[word.lower() for word in sent if word not in remove_terms] for sent in bible]
 norm_bible = [' '.join(tok_sent) for tok_sent in norm_bible]
+norm_bible = filter(None, normalize_corpus(norm_bible))
+norm_bible = [tok_sent for tok_sent in norm_bible if len(tok_sent.split()) > 2]
 
 tokenizer = text.Tokenizer()
 tokenizer.fit_on_texts(norm_bible)
+word2id = tokenizer.word_index
+
+word2id['PAD'] = 0
+id2word = {v:k for k, v in word2id.items()}
+wids
+
+
+
+pd.options.display.max_colwidth = 200
+
+corpus = ['The sky is blue and beautiful.',
+          'Love this blue and beautiful sky!',
+          'The quick brown fox jumps over the lazy dog.',
+          "A king's breakfast has sausages, ham, bacon, eggs, toast and beans",
+          'I love green eggs, ham, sausages and bacon!',
+          'The brown fox is quick and the blue dog is lazy!',
+          'The sky is very blue and the sky is very beautiful today',
+          'The dog is lazy but the brown fox is quick!'
+]
+
+labels = ['weather', 'weather', 'animals', 'food', 'food', 'animals', 'weather', 'animals']
+
+corpus = np.array(corpus)
+corpus_df = pd.DataFrame({'Document': corpus, 'Category': labels})
+corpus_df = corpus_df[['Document', 'Category']]
+corpus_df
+
